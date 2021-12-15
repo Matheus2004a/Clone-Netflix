2ffd60b69177730fdc9c48628d6f27e83864d182
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-12-15 14:25:14 -0300

    Adicionando seção de mais assistidos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const buttonBackToTop = document.querySelector(".btn-back-to-top")
 
 window.addEventListener("scroll", () => {
-    scrollY >= 450 ? buttonBackToTop.classList.add("show") : buttonBackToTop.classList.remove("show")
+    scrollY >= 650 ? buttonBackToTop.classList.add("show") : buttonBackToTop.classList.remove("show")
 })
