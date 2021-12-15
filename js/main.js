892c18fe93bbbc92d6eb6a888d153119d1a30302
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-12-15 15:31:59 -0300

    Desabilitando áudio e retornando vídeo ao começo do trailer da série do slide

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+const iframeModal = document.querySelector("iframe")
+const btnClose = document.querySelector(".btn-close")
+
+btnClose.addEventListener("click", () => {
+    const url = iframeModal.getAttribute("src")
+    iframeModal.setAttribute("src", "")
+    iframeModal.setAttribute("src", url)
+})
+
 const buttonBackToTop = document.querySelector(".btn-back-to-top")
 
 window.addEventListener("scroll", () => {
