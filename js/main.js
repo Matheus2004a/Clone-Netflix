c6e90873c28f544235cbc4ece73a4c9e6a4f3a63
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-12-15 11:18:08 -0300

    Corrigindo animação do botão de voltar ao topo e infos do modal do filme

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
-const closeModalPlayMovie = document.querySelector(".close-modal")
-const videoYt = document.querySelector("#video-slide")
-
-closeModalPlayMovie.addEventListener("click", () => {
-    modalMovie.classList.toggle("active")
-})
-
-let iframeVideo = videoYt.contentWindow
-console.log(iframeVideo)
-
 const buttonBackToTop = document.querySelector(".btn-back-to-top")
 
 window.addEventListener("scroll", () => {
-    scrollY >= 380 ? buttonBackToTop.classList.add("show") : buttonBackToTop.classList.remove("show")
+    scrollY >= 450 ? buttonBackToTop.classList.add("show") : buttonBackToTop.classList.remove("show")
 })
