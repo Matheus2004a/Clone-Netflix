5c0f481396cdcb3f24434e6ab0e520f04f364259
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-12-16 13:18:52 -0300

    Removendo áudio do vídeo ao clicar fora do modal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const iframeModal = document.querySelector("iframe")
+const modalVideo = document.querySelector(".modal")
 const btnClose = document.querySelector(".btn-close")
 
-btnClose.addEventListener("click", () => {
+btnClose.addEventListener("click", disabledAudioVideo)
+modalVideo.addEventListener("click", disabledAudioVideo)
+
+function disabledAudioVideo() {
     const url = iframeModal.getAttribute("src")
     iframeModal.setAttribute("src", "")
     iframeModal.setAttribute("src", url)
-})
+}
 
 const buttonBackToTop = document.querySelector(".btn-back-to-top")
 
